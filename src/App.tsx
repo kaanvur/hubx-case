51884e73d515ad494db06ac5b60f64d94c6c4b40
kaanvur <lastcain@gmail.com>
2024-02-09 12:54:07 +0300

    preload image

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Menu from './components/Menu';
-
+import { IMAGES } from "./Images"
 import AnimatedRouter from './components/AnimatedRouter';
 
 const App: React.FC = () => {
+type ImageType = {
+  url: string; 
+  alt?: string;
+};
+    const [imgsLoaded, setImgsLoaded] = useState(false)
+
+  useEffect(() => {
+    const loadImage = (image: ImageType) => {
+      return new Promise((resolve, reject) => {
+        const loadImg = new Image()
+        loadImg.src = image.url
+        resolve(image.url)
+
+        loadImg.onerror = err => reject(err)
+      })
+    }
+
+    Promise.all(IMAGES.map(image => loadImage(image)))
+      .then(() => setImgsLoaded(true))
+      .catch(err => console.log("Failed to load images", err))
+  }, [])
+
   return (
-    <Router>
+    <>
+      {imgsLoaded ? (
+              <Router>
       <div className="App app">
         <AnimatedRouter />
         <Menu />
       </div>
     </Router>
+        ) : (
+          <h2 style={{textAlign: 'center'}}>Loading images...</h2>
+        )}
+    </>
+
   );
 };
 
---
+++ src/Images.tsx
@@ -0,0 +1,82 @@
+export const IMAGES = [
+    {
+        id: 0,
+        url:
+            "/src/img/img-AdvFilters.png"
+    },
+    {
+        id: 1,
+        url:
+            "/src/img/img-AdvFilters-1.png"
+    },
+    {
+        id: 2,
+        url:
+            "/src/img/img-AdvFilters-2.png"
+    },
+    {
+        id: 3,
+        url:
+            "/src/img/img-BatchScan.png"
+    },
+{
+        id: 4,
+        url:
+            "/src/img/img-BatchScan-1.png"
+    },
+    {
+        id: 5,
+        url:
+            "/src/img/img-BatchScan-2.png"
+    },
+    {
+        id: 6,
+        url:
+            "/src/img/img-BatchScan-3.png"
+    },
+    {
+        id: 7,
+        url:
+            "/src/img/img-DocScanner.png"
+    },
+    {
+        id: 8,
+        url:
+            "/src/img/img-ExportShare.png"
+    },
+    {
+        id: 9,
+        url:
+            "/src/img/img-ExportShare-1.png"
+    },
+    {
+        id: 10,
+        url:
+            "/src/img/img-ExportShare-2.png"
+    },
+    {
+        id: 11,
+        url:
+            "/src/img/img-ExportShare-3.png"
+    },
+    {
+        id: 12,
+        url:
+            "/src/img/img-ExportShare-4.png"
+    },
+    {
+        id: 13,
+        url:
+            "/src/img/img-SignStamp.png"
+    },
+    {
+        id: 14,
+        url:
+            "/src/img/img-SignStamp-1.png"
+    },
+    {
+        id: 15,
+        url:
+            "/src/img/img-SignStamp-2.png"
+    },
+]
